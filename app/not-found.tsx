10accbb615d48d0dfa83f454c39f0d3738739681
Yulia Bezsmertna <yuliabezsmertna80@gmail.com>
2025-09-16 16:28:46 +0300

    create and correct

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,26 +1,19 @@
-"use client";
-
-import type { Metadata } from 'next';
-import css from './not-found.module.css';
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: '404 - Page not found | NoteHub',
-  description: 'Ця сторінка не існує в NoteHub',
+  title: "404 - Page not found",
+  description: "Sorry, the page you are looking for does not exist.",
   openGraph: {
-    title: '404 - Page not found | NoteHub',
-    description: 'Ця сторінка не існує в NoteHub',
-    url: 'https://your-vercel-url.vercel.app/404',
-    images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
+    title: "404 - Page not found",
+    description: "Sorry, the page you are looking for does not exist.",
+    url: "https://08-zustand-gules-mu.vercel.app/404",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "404 - Page not found",
+      },
+    ],
   },
 };
-
-export default function NotFoundPage() {
-  return (
-    <main className={css.main}>
-      <h1 className={css.title}>404 - Page not found</h1>
-      <p className={css.description}>
-        Sorry, the page you are looking for does not exist.
-      </p>
-    </main>
-  );
-}
