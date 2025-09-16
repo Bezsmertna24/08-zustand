4bf1416f33a7fa9d4aef49ae8d17b103e50241ba
Yulia Bezsmertna <yuliabezsmertna80@gmail.com>
2025-09-16 16:41:58 +0300

    correct

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,19 @@
+"use client";
 import { Metadata } from "next";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import css from "./Home.module.css";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className={css.title}>404 - Page not found</h1>
+      <p className={css.description}>
+        Sorry, the page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
 
 export const metadata: Metadata = {
   title: "404 - Page not found",
