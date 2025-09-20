6fd936f851fc62102ddbeb5adb1c7edf0731fc02
Yulia Bezsmertna <yuliabezsmertna80@gmail.com>
2025-09-20 22:17:40 +0300

    correct

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,13 @@
-"use client";
 import { Metadata } from "next";
+import css from "./not-found.module.css";
+
 
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 - Page not found</h1>
-      <p>
+    <div className={css.container}>
+      <h1 className={css.title}>404 - Page not found</h1>
+      <p> className={css.description}
         Sorry, the page you are looking for does not exist.
       </p>
     </div>
